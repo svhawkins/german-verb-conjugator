7ab427c324613afe9619464d501e66bcc1ba0ccc
sandra <sandravhawkins@gmail.com>
2024-04-01 20:55:36 -0400

    add subclasses

--- verbs.py
+++ verbs.py
@@ -3,11 +3,22 @@ Verbs
 
 Module containing verb classes necessary to conjugate a German verb. 
 
-Consists of only a single class: Verb.
-    This class handles strong, weak, mixed, irregular, preterite-present verbs (so all verbs pretty much)
-The Verb class does most of the work in the conjugations.
-A Verb is constructed from its infinitive an 'is_weak' indicator, and an 'is_transitive' indicator.
+Consists of the base class Verb and the following subclasses:
 
+1. Strong --> for Strong and Irregular verbs (stem changes in present and past)
+2. Weak --> for verbs that take 'regular' endings (no stem changes)
+3. Mixed --> Mixed and preterite-present verbs (stem changes and 'regular' endings)
+
+The Verb class does most of the work in the conjugations.
+A Verb is constructed from the following:
+    - Its infinitive
+	- Haben/Sein perfect aspect auxiliary indicator
+	- (Optional) Prefix and whether it is separable
+	- (Optional) Parts to override default values from Verbs dataframe:
+            - infinitive : TODO: make as present stem instead?
+            - past stem
+            - past participle
+            - present endings (irregular and preterite presents)
 """
 
 import re
@@ -26,7 +37,7 @@ class Number(IntEnum):
 
 class Tense(IntEnum): 
 	PRESENT = 0
-	IMPERFECT = 1
+	PAST = 1
 	FUTURE = 2
 	
 class Aspect(IntEnum):
@@ -36,15 +47,40 @@ class Aspect(IntEnum):
 class Mood(IntEnum):
 	INDICATIVE = 0
 	IMPERATIVE = 1 # present only
-	SUBJUNCTIVE = 2 # future subjunctive 2 is the conditional
+	SUBJUNCTIVE_1 = 2 # future has 2 subjunctives, present stem subjunctive
+	SUBJUNCTIVE_2 = 3 # future has 2 subjunctives, past stem subjunctive
+                      # past stem subjunctive for future is the conditional
 	
-n_entries = len(Tense) * len(Mood) * len(Aspect) * len(Person) * len(Number)
-	
-# lambdas
-is_none_value = lambda tense, mood, aspect, person, number: ( False ) # TODO
+
+# imperative is present indicative simple only, 2nd person sg/pl or 1st person pl
+# subjunctive_1 is future and present only
+# subjunctive_2 is future and past only
+def is_none_value(tense : int, mood : int, aspect : int, person : int, number : int) -> bool:
+	valid_imperative = (tense == Tense.PRESENT) and \
+                       (mood == Mood.IMPERATIVE) and \
+					   (aspect == Aspect.SIMPLE) and \
+					   ((person == Person.SECOND) or (person == Person.FIRST and number == Number.PLURAL))
+	valid_subjunctive = ((mood == Mood.SUBJUNCTIVE_1) and (tense == Tense.PRESENT)) or \
+                        ((mood == Mood.SUBJUNCTIVE_2) and (tense == Tense.PAST)) or \
+						(tense == Tense.FUTURE)
+	return (not valid_imperative) or (not valid_subjunctive)
 
 # hash function
-table_hash = lambda tense, mood, aspect, person, number: ( 0 ) # TODO
+# offsets
+n_entries = len(Tense) * len(Mood) * len(Aspect) * len(Person) * len(Number) # 3 * 4 * 2 * 3 * 2 = 144
+MOOD_OFFSET = n_entries / len(Mood) # 144 / 4 = 36
+TENSE_OFFSET = MOOD_OFFSET / len(Tense) # 36 / 3 = 12
+ASPECT_OFFSET = TENSE_OFFSET / len(Aspect) # 12 / 2 = 6
+PERSON_OFFSET = ASPECT_OFFSET / len(Person) # 6 / 3 = 2
+NUMBER_OFFSET = PERSON_OFFSET / len(Number) # 2 / 2 = 1
+
+def table_hash(mood : int, tense : int, aspect : int, person : int, number : int) -> int: 
+    index = (MOOD_OFFSET * mood) + \
+    (TENSE_OFFSET * tense) + \
+    (ASPECT_OFFSET * aspect) + \
+    (PERSON_OFFSET * person) + \
+    (NUMBER_OFFSET * number)
+    return index
 
 
 class Verb:
@@ -56,62 +92,69 @@ class Verb:
 	Class members:
 		_empty : tuple --> empty/default endings
 		_present_endings : tuple --> default present tense endings
-		_past_endings : tuple ---> default imperfect tense endings
-		_subjunctive_endings : tuple --> default subjunctive mood endings
+		_past_endings : tuple ---> default past tense endings
+		_subjunctive1_endings : tuple --> default subjunctive 1 mood endings
+		_subjunctive2_endings : tuple --> default subjunctive 2 mood endings
 
 	Attributes:
 		infinitive : str --> a verb's infinitive
 		stem : str --> a verb's stem (infinitive sans the ending)
 		past_stem : str --> a verb's past tense stem
 		participle : str --> a verb's (past) participle
+		imperative_stem : str --> a verb's stem for imperative mood
+		subjunctive1_stem : str --> a verb's stem for present-stem-based subjunctive (Konjuktiv I)
+		subjunctive2_stem : str --> a verb's stem for past-stem-based subjunctive (Konjuktiv II)
 	
 	Methods:
-		conjugate(self, tense_idx : int = len(Tense), person_idx : int = len(Person))
+		conjugate(self, tense : int = len(Tense),
+                        mood: int = len(Mood),
+						aspect : int = len(Aspect),
+		                person: int = len(Person),
+						number : int = len(Number))
 			conjugate a verb
-		get_conjugation_at(self, tense_idx : int, person_idx : int) -> str
-			get specified conjugation
+		get_conjugation_at(self, tense : int, mood : int, aspect : int, person : int, number : int) -> str
+			get specified conjugation with specified qualities (index)
 		clear_table(self)
 			clear conjugation table
 		get_table(self)
 			get conjugation table
 	"""
-	# static/protected class members --> constant for ALL verbs pretty much unless aspect stuff appears
+	# static/protected class members --> constant for ALL verbs
 
-	# past participle endings: 1st singular, 2nd singular, 3rd singular, 1st plural, 2nd plural, 3rd plural
-	_empty = ("", "", "", "", "", "")
-	_present_endings = ("e", "st", "t", "en", "t", "en") # also stong past endings 
-	_past_endings = ("te, test, te, ten, tet, ten") # weak endings
-	_subjunctive_endings = ("e", "est", "e", "en", "et", "en") # basically weak endings sans initial -t
+	_empty = ["", "", "", "", "", ""]
+	_present_endings = ["e", "st", "t", "en", "t", "en"]
+	_past_endings = _empty # dependent on class
+	_subjunctive1_endings = ["e", "est", "e", "en", "et", "en"]
+	_subjunctive2_endings = _empty # dependent on class
 
-	# tense to ending mappings
-
-	def __init__(self, infinitive : str = "", ending : str = "",
-                 is_weak : bool = True, is_transitive : bool = True,
-				 present_endings : tuple = ()):
+	def __init__(self, infinitive : str = "",
+                 use_haben : bool = True,
+			     prefix : tuple = ("", False),
+				 parts = {}):
 		"""
 		Construct a Verb object.
 
 		Parameters:
 			infinitive (default "") : str --> infinitive of the verb
-			ending (default "") : str --> the verb's ending
-			is_weak (default True) : bool --> indicator if a verb is weak (determines endings used)
-			is_transitive (default True) : bool --> indicator if a verb is transitive (determines perfect auxiliary)
-			present_endings (default empty) : tuple --> passed in tuple to override indicative present tense conjugation
+			use_haben (default True) : bool --> indicator if a verb uses haben or sein in perfect aspect
+			prefix (default ("", False)) : tuple --> passed in tuple to indicate prefix portion and whether it is separable
+			parts (default {}) : dict str : str --> passed in parts to override default stems
 		"""
-		# stems (public)
+		# stems
 		self.infinitive = infinitive
-		self.ending = ending
-		self.stem = ""
-		self.past_stem = ""
+		self.stem = "" # derived in derived classes
+		self.past_stem = "" # derived in derived classes
+		self.participle = "" # derived in derived classes
+		self.imperative_stem = ""  # later derived
+		self.subjunctive1_stem = "" # later derived
+		self.subjunctive2_stem = "" # later derived
 		
+        # prefix
+		self._prefix = prefix[0]
+		self._is_separable = prefix[1]
+
         # flags
-		self._override_present = len(present_endings) != 0
-		self._present_endings = present_endings if self._override_present else self._present_endings
-		self._is_transitive = is_transitive
-		self._is_weak = is_weak
-		
-		# if not is_weak: # overrides default weak conjugations in affected tenses + moods
-		#     self._is_weak = False
+		self._use_haben = use_haben
 
 		# conjugation table
 		self._conjugation_table = ["" for person in range(n_entries)]
@@ -149,7 +192,7 @@ class Verb:
 		"""
 		
         # TODO:
-		# make (recursive) function to get list of hashes
+		# make (recursive? idk maybe iterative) function to get list of hashes
 		indices = []
 		for index in indices:
 			self._conjugation_table[index] = self._get_conjugation(tense, mood, aspect, person, number)
@@ -181,3 +224,93 @@ class Verb:
 	def kind(self) -> str:
 		"""Return type of class as string"""
 		return "Verb"
+	
+class Strong(Verb):
+	"""Class for strong verbs"""
+
+	# verb class specific endings:
+	# conjugations use same endings as present-tense moods, though there is ablaut in stems.
+	_past_endings = ("", "st", "", "en", "et", "en")
+	_subjunctive2_endings = ("e", "est", "e", "en", "et", "en")
+
+	def __init__(self, infinitive : str = "",
+                 use_haben : bool = True,
+			     prefix : tuple = ("", False),
+				 parts = {}):
+		super().__init__(infinitive, use_haben, prefix, parts)
+		
+        # get stems. they are non-derivable.
+		if len(parts) > 0:
+			self.stem = parts["present"][:-2] # remove the [ei]n
+			self.past_stem = parts["past"]
+			self.participle = parts["participle"]
+			if len(parts["conjugation"] > 0):
+				self._present_endings = parts["conjugation"]
+			
+		self.subjunctive1_stem = self.stem
+		self.subjunctive2_stem = self.past_stem
+		self.imperative_stem = self.stem
+		
+        # TODO: account for umlauts
+
+	def kind(self) -> str:
+		"""Return type of class as string"""
+		return "Strong"
+	
+class Weak(Verb):
+	"""Class for weak verbs"""
+
+	# verb class specific endings:
+	_past_endings = ("te, test", "te", "ten", "tet", "ten")
+	_subjunctive2_endings = _past_endings
+
+	def __init__(self, infinitive : str = "",
+                 use_haben : bool = True,
+			     prefix : tuple = ("", False),
+				 parts = {}):
+		super().__init__(infinitive, use_haben, prefix, parts)
+		
+        # derive stems
+		self.stem = self.infinitive[:-2] # TODO: not always last 2
+		self.past_stem = self.stem # TODO: = -(e)ten verbs and few others have extra rules
+		self.participle = "ge" + self.past_stem + "t" # TODO: not all have participles like this
+		self.imperative_stem = self.stem
+		self.subjunctive1_stem = self.stem
+		self.subjunctive2_stem = self.past_stem
+		
+        # TODO: account for umlauts
+
+	def kind(self) -> str:
+		"""Return type of class as string"""
+		return "Weak"
+	
+class Mixed(Verb):
+	"""Class for mixed and preterite present verbs"""
+
+	# verb class specific endings:
+	_past_endings = ("te, test", "te", "ten", "tet", "ten")
+	_subjunctive2_endings = _past_endings
+
+	def __init__(self, infinitive : str = "",
+                 use_haben : bool = True,
+			     prefix : tuple = ("", False),
+				 parts = {}):
+		super().__init__(infinitive, use_haben, prefix, parts)
+		
+        # get stems. they are non-derivable.
+		if len(parts) > 0:
+			self.stem = parts["present"][:-2] # remove the [ei]n
+			self.past_stem = parts["past"]
+			self.participle = parts["participle"]
+			if len(parts["conjugation"] > 0):
+				self._present_endings = parts["conjugation"]
+			
+		self.subjunctive1_stem = self.stem
+		self.subjunctive2_stem = self.past_stem
+		self.imperative_stem = self.stem
+		
+        # TODO: account for umlauts
+
+	def kind(self) -> str:
+		"""Return type of class as string"""
+		return "Mixed"
